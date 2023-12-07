30b426e724fcc821d41f6825877975b11349a77d
DerickMghanga <derickmghanga3329@gmail.com>
2023-12-07 20:08:50 +0300

    URL Configuration

--- chefsTable/myapp/views.py
+++ chefsTable/myapp/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello there !")
+    return HttpResponse("Welcome to the Little Lemon Restaurant !")
