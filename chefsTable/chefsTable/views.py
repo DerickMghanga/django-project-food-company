7bec37066c473be8e4ca4ce5ec7ebf24b2e9cefe
DerickMghanga <derickmghanga3329@gmail.com>
2023-12-13 00:02:14 +0300

    project corrected

--- chefsTable/chefsTable/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse, HttpResponseNotFound
-
-def handler404(request, exception):
-    return HttpResponse("404: Page Not Found")
-
-# custom error displayed at the inspect tools
-def lemon(request):
-    return HttpResponseNotFound("Little lemon")
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for chefsTable project.
+URL configuration for myproject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,16 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
-from . import views
-
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),
-    path('lemon', views.lemon),
 ]
-
-# handles pages not found errors from views.py(project)
-handler404 = 'chefsTable.views.handler404'
