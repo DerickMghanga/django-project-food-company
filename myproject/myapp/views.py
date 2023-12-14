37bd356cb0a581b017d2478fbf5c18b5554ded9b
DerickMghanga <derickmghanga3329@gmail.com>
2023-12-14 14:23:38 +0300

    creating templates

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def about(request):
+    about_content = {'about': 'Based in Mombasa, Kenya, a family owned swahili restaurant. Offering moderate prices, making it a popular place for a meal any time of the day.'}
+    return render(request, 'about.html', about_content)
